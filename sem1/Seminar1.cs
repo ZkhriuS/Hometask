0821f4d68fd3f979d727a335ee106a64f2125fa4
Zakhar <seriy.zachar@yandex.by>
2022-08-27 21:42:44 +0300

    task 2

--- sem1/Seminar1.cs
+++ sem1/Seminar1.cs
@@ -1,6 +1,10 @@
-﻿/*
-Задача 2 находится в ветке task2
-Задача 4 находится в ветке task4
-Задача 6 находится в ветке task6
-Задача 8 находится в ветке task8
-*/
+﻿Console.WriteLine("Enter 2 values: ");
+if (double.TryParse(Console.ReadLine(), out double first) && double.TryParse(Console.ReadLine(), out double second))
+{
+    if (first > second) Console.WriteLine($"max = {first}, min ={second}");
+    else Console.WriteLine($"max = {second}, min ={first}");
+}
+else
+{
+    Console.WriteLine("Incorrect input");
+}
