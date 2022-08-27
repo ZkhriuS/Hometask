2b4740f4da230d55b281172b1b2c0f4c8405e12c
Zakhar <seriy.zachar@yandex.by>
2022-08-27 22:32:52 +0300

    task 13

--- sem2/Seminar2.cs
+++ sem2/Seminar2.cs
@@ -1,5 +1,5 @@
-﻿int value = new Random().Next(100000);
+﻿int value = new Random().Next();
 int power = 1;
-while (value / power > 0) power *= 10;
-if (value > 99) Console.WriteLine($"third digit of {value} is {value / (power / 1000) % 10}");
+while (value / power > 1000) power *= 10;
+if (value > 99) Console.WriteLine($"third digit of {value} is {value / power % 10}");
 else Console.WriteLine($"there isn't third digit in {value}");
