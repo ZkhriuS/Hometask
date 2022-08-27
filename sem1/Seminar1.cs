0c5658710061966b108c62f851ef7c925bd203ff
Zakhar <seriy.zachar@yandex.by>
2022-08-27 21:39:06 +0300

    task 4

--- sem1/Seminar1.cs
+++ sem1/Seminar1.cs
@@ -1,6 +1,14 @@
-﻿/*
-Задача 2 находится в ветке task2
-Задача 4 находится в ветке task4
-Задача 6 находится в ветке task6
-Задача 8 находится в ветке task8
-*/
+﻿Console.WriteLine("Enter 3 values: ");
+if (double.TryParse(Console.ReadLine(), out double first)
+    && double.TryParse(Console.ReadLine(), out double second)
+    && double.TryParse(Console.ReadLine(), out double third))
+{
+    double max = first;
+    if (second > max) max = second;
+    if (third > max) max = third;
+    Console.WriteLine($"max = {max}");
+}
+else
+{
+    Console.WriteLine("Incorrect input");
+}
